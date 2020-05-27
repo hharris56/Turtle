e1b0ac9f80a9f6f4aaba03f0396d20ff1500a0c4
hunterxps <hvharris@calpoly.edu>
2020-05-27 03:25:28 -0700

    prompt, refactor, .turtlerc

--- shell.py
+++ shell.py
@@ -29,34 +29,57 @@ import subprocess
 # =====================================================================
 
 def main():
-	while True:
-		userin = input("-> ")
-		if userin == "exit":
-			break
-		elif userin == "help":
-			print("HELP MESSAGE")
-		else:
-			excecute(userin)
+
+	startup()
+	try:
+		use()
+	except KeyboardInterrupt:
+		pass
+
+	print("\nExiting turtle... goodbye :)")
 
 # =====================================================================
 # Helper Methods
 # =====================================================================
 
-def excecute(command):
-	# parse_input(userinput)	
+def startup():
+	rc = "assets/.turtlerc"
+	if os.path.isfile(rc):
+		subprocess.run("./{}".format(rc))
+	else:
+		print("no rc file found")
+
+def use():
+	cwd = os.getcwd()
+
+	while True:
+		command = input("\n{} -> ".format(cwd))
+		opt = parseInput(command)
+	
+		# exit
+		if opt == -1:
+			break
+
+		# execute command
+		elif opt == 0:
+			execute(command)
+
+def execute(command):
 	try:
 		subprocess.run(command.split())
 	except:
 		print("ERROR Command not found : {}".format(command));
 
-def parse_input(userin):
-	pass
-
+def parseInput(command):
+	rawin = command.lower()
+	if rawin == "exit" or rawin == "e":
+		return -1
+	elif rawin == "help":
+		print("INSERT HELP MESSAGE HERE\n")
+	else:
+		return 0
 
 
 
 if __name__ == "__main__":
-	try:	
-		main()
-	except KeyboardInterrupt:
-		exit()
+	main()
