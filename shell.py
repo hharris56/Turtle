b8b89b871ad96cab5eded05abe55d5cc40d3dfbf
hunterxps <hvharris@calpoly.edu>
2020-05-27 14:06:31 -0700

    removed prompt cwd, readme changes

--- shell.py
+++ shell.py
@@ -2,15 +2,11 @@
 # May 27 2020
 # Basis Python Shell
 
-# DEV THOUGTS
+# DEV COMMENTS
 """
 cd is not a 'command', must impliment a way to change current
 working directory
 	- will need os
-
-
-
-
 
 """
 
@@ -42,6 +38,7 @@ def main():
 # Helper Methods
 # =====================================================================
 
+# processes to be completed upon shell launch
 def startup():
 	rc = "assets/.turtlerc"
 	if os.path.isfile(rc):
@@ -49,28 +46,33 @@ def startup():
 	else:
 		print("no rc file found")
 
+# main shell loop
+# gets user input, passes to parseInput and possibly execute
 def use():
-	cwd = os.getcwd()
-
+	#cwd = os.getcwd()
 	while True:
-		command = input("\n{} -> ".format(cwd))
+		#command = input("\n{} -> ".format(cwd))
+		command = input("\nprompt -> ")
 		opt = parseInput(command)
-	
 		# exit
 		if opt == -1:
 			break
-
 		# execute command
 		elif opt == 0:
 			execute(command)
 
+# executes the given command using subprocess.run
 def execute(command):
 	try:
 		subprocess.run(command.split())
 	except:
 		print("ERROR Command not found : {}".format(command));
 
+# parses user input
+# return -1 to exit
+# return 0 to execute
 def parseInput(command):
+	# check for key phrases
 	rawin = command.lower()
 	if rawin == "exit" or rawin == "e":
 		return -1
